b8c25ac3d4368bf20f2bbd1646f4fd978edb9339
Duong Tung Anh <tunnaduong@gmail.com>
2024-03-05 14:46:02 +0700

    Update param.tsx

--- pages/api/param.tsx
+++ pages/api/param.tsx
@@ -9,67 +9,70 @@ export default function handler(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url)
 
-    // ?title=<title>
-    const hasTitle = searchParams.has('title')
-    const title = hasTitle
-      ? searchParams.get('title')?.slice(0, 100)
-      : 'My default title'
+// ?name=<name>
+const hasName = searchParams.has('name')
+const name = hasName
+  ? searchParams.get('name')?.slice(0, 100)
+  : ''
+    
+// ?title=<title>
+const hasTitle = searchParams.has('title')
+const title = hasTitle
+  ? searchParams.get('title')?.slice(0, 100)
+  : ''
 
+
+if (title != "" && name != "") {
     return new ImageResponse(
       (
         <div
-          style={{
-            backgroundColor: 'black',
-            backgroundSize: '150px 150px',
-            height: '100%',
-            width: '100%',
-            display: 'flex',
-            textAlign: 'center',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              alignContent: 'center',
-              justifyContent: 'center',
-              justifyItems: 'center',
-            }}
-          >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              alt="Vercel"
-              height={200}
-              src="data:image/svg+xml,%3Csvg width='116' height='100' fill='white' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M57.5 0L115 100H0L57.5 0z' /%3E%3C/svg%3E"
-              style={{ margin: '0 30px' }}
-              width={232}
-            />
-          </div>
-          <div
-            style={{
-              fontSize: 60,
-              fontStyle: 'normal',
-              letterSpacing: '-0.025em',
-              color: 'white',
-              marginTop: 30,
-              padding: '0 120px',
-              lineHeight: 1.4,
-              whiteSpace: 'pre-wrap',
-            }}
-          >
-            {title}
-          </div>
-        </div>
+  style={{
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    fontSize: 32,
+    padding: 35,
+    textAlign: 'center'
+  }}
+>
+  <b>{name}</b> vừa hoàn thành bài test tâm lý với kết quả<br />{title}. <br /><span style={{color:'green'}}>Bấm vào đây để test ngay!</span><br /><p style={{fontSize: 17, marginBottom: 0}}>© {new Date().getFullYear()} TUNNA DUONG & TRUNG TÂM PHỤC HỒI CHỨC NĂNG TÂM THẦN</p>
+</div>
       ),
       {
-        width: 1200,
-        height: 630,
+        width: 800,
+        height: 400,
       }
     )
+} else {
+  return new ImageResponse(
+      (
+        <div
+  style={{
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    fontSize: 32,
+    padding: 35,
+    textAlign: 'center'
+  }}
+>
+Khảo sát phát hiện sớm dấu hiệu<br />trầm cảm miễn phí!<br /><span style={{color:'green'}}>Bấm vào đây để test ngay!</span><br /><p style={{fontSize: 17, marginBottom: 0}}>© {new Date().getFullYear()} TUNNA DUONG & TRUNG TÂM PHỤC HỒI CHỨC NĂNG TÂM THẦN</p>
+</div>
+      ),
+      {
+        width: 800,
+        height: 400,
+      }
+    )
+}
   } catch (e: any) {
     console.log(`${e.message}`)
     return new Response(`Failed to generate the image`, {
